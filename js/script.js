a8bbd2de0916ca6311db8893c8ae6ba6239d8e5d
Abdikarim B <40158936+AbdikarimBuraleh@users.noreply.github.com>
2024-03-05 01:30:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,26 +46,20 @@ form.addEventListener("submit", async (event) => {
         alert("There was a problem sending your message. Please try again.");
     }
 });
-// Get the modal
+
+// Modal functionality
 const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 const btn = document.getElementById("start-button");
-
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
